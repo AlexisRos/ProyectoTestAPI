25c3977a633dc628f170619c562afd1d3b43a6dd
AlexisRos55 <159597577+AlexisRos@users.noreply.github.com>
2025-11-22 01:24:14 -0600

    Update app.config.ts

--- proyecto_prueba_api/src/app/app.config.ts
+++ proyecto_prueba_api/src/app/app.config.ts
@@ -3,12 +3,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; 
-import { routes } from './app.routes'; // Asumimos que esta ruta está bien
+import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // ⬇️ Estos son los proveedores mínimos necesarios para el lado del cliente ⬇️
     provideRouter(routes), // Proporciona el enrutamiento para el navegador
     provideHttpClient(),    // Proporciona el cliente HTTP
   ]
+
 };
